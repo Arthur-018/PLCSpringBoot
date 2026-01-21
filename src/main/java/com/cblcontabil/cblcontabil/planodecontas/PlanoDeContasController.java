31a9d1708c36f75f89b19282099a5fcb704b7a2c
devramoos <rodrigojgramos@outlook.com>
2026-01-21 13:13:14 -0300

    refactor: Refatora o controller para incluir responseIntity em todas as rotas.

--- src/main/java/com/cblcontabil/cblcontabil/planodecontas/PlanoDeContasController.java
+++ src/main/java/com/cblcontabil/cblcontabil/planodecontas/PlanoDeContasController.java
@@ -1,6 +1,9 @@
 package com.cblcontabil.cblcontabil.planodecontas;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,41 +20,64 @@ public class PlanoDeContasController {
     }
 
     @GetMapping
-    public String boasVindas(){
+    public String boasVindas() {
         return "Boas vindas";
     }
 
     @PostMapping("/criar")
-    public PlanoDeContasDTO criarContas(@RequestBody PlanoDeContasDTO planoDeContasDTO){
-        return planoDeContasService.criarConta(planoDeContasDTO);
+    public ResponseEntity<String> criarContas(@RequestBody PlanoDeContasDTO conta) {
+        PlanoDeContasDTO novaConta = planoDeContasService.criarConta(conta);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Conta criada com sucesso: " + novaConta.getDescricao() + " (Código): " + novaConta.getCodigo());
     }
 
     @GetMapping("/listar")
-    public List<PlanoDeContasDTO> listarConta(){
-        return planoDeContasService.listarConta();
+    public ResponseEntity<List<PlanoDeContasDTO>>  listarConta() {
+        List<PlanoDeContasDTO> contas = planoDeContasService.listarConta();
+        return ResponseEntity.ok(contas);
     }
 
     @GetMapping("/buscarcontaporcodigo/{codigo}")
-    public PlanoDeContasDTO buscarPorCodigo(@PathVariable int codigo) {
-        return planoDeContasService.buscarContaPorCodigo(codigo)
-                .orElse(null);
-    }
-
-    @PutMapping("/alterarconta/{codigo}")
-    public PlanoDeContasDTO alterarConta(@PathVariable int codigo, @RequestBody PlanoDeContasDTO planoDeContasDTO){
-        return planoDeContasService.atualizarConta(codigo, planoDeContasDTO);
-    }
-
-    @DeleteMapping("/deletarconta/{codigo}")
-    public void deletarConta(@PathVariable int codigo){
-        PlanoDeContasDTO conta = planoDeContasService.buscarContaPorCodigo(codigo)
+    public ResponseEntity<?> buscarPorCodigo(@PathVariable int codigo) {
+        PlanoDeContasDTO contaCodigo = planoDeContasService.buscarContaPorCodigo(codigo)
                 .orElse(null);
 
-        if (conta != null) {
-            planoDeContasService.deletarPorCodigo(codigo);
-        } else {
-            throw new RuntimeException("Conta não encontrada");
+        if (contaCodigo != null){
+            return ResponseEntity.ok(contaCodigo);
+
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Conta com o id: " + codigo + " não encontrada nos nossos registros.");
         }
     }
 
+    @PutMapping("/alterarconta/{codigo}")
+    public ResponseEntity<?> alterarConta(@PathVariable int codigo, @RequestBody PlanoDeContasDTO planoDeContasDTO) {
+
+        PlanoDeContasDTO conta = planoDeContasService.atualizarConta(codigo, planoDeContasDTO);
+
+        if (conta != null){
+            return ResponseEntity.ok(conta);
+
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Conta com o id: " + codigo + " não encontrada nos nossos registros.");
+        }
+
+    }
+
+    @DeleteMapping("/deletarconta/{codigo}")
+        public ResponseEntity<String> deletarConta(@PathVariable int codigo) {
+        PlanoDeContasDTO conta = planoDeContasService.buscarContaPorCodigo(codigo)
+            .orElse(null);
+        if (planoDeContasService.buscarContaPorCodigo(codigo) != null){
+            planoDeContasService.deletarPorCodigo(codigo);
+            return ResponseEntity.ok("Conta com o código " + codigo + " excluída com sucesso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Conta com o código " + codigo + "não encontrada.");
+        }
+
+
+    }
 }
